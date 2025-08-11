580425e9e92194e58d5302d0225f1e02de1a19da
Aditya Singh Rathore <aditya.singh.rathore@intel.com>
2025-08-11 15:32:50 +0000

    Initial setup

---
+++ gsplat/sycl/src/adam.cpp
@@ -0,0 +1,15 @@
+#include "Ops.h"
+
+void adam(
+    at::Tensor &param,                    // [..., D]
+    const at::Tensor &param_grad,         // [..., D]
+    at::Tensor &exp_avg,                  // [..., D]
+    at::Tensor &exp_avg_sq,               // [..., D]
+    const at::optional<at::Tensor> valid, // [...]
+    const float lr,
+    const float b1,
+    const float b2,
+    const float eps
+) {
+
+}
